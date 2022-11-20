64514a8413a1f04ff6c9b47b8d183e85c230a563
Yunus Emre Şahin <yunus.bjk123@hotmail.com>
2022-11-20 23:10:42 +0300

    Generics,Constructor

---
+++ GenericIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+//List<string> items = new List<string> { "murat", "hasan", "kemal", "arif" };
+
+//Console.WriteLine(items[0]);
+//Console.WriteLine(items[1]);
+//Console.WriteLine(items[2]);
+//items.Add("mahmut");
+//Console.WriteLine(items[4]);
+
+List<string> items = new List<string> ();
+
+Console.WriteLine("Hasan");
+Console.WriteLine("Mahmut");
+Console.WriteLine("Arif");
+Console.WriteLine("eee");
+
+
+
+//foreach (var item in items)
+//{
+//    Console.WriteLine(item);
+//}
+
